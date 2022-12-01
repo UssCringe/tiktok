8e1b6759642dd610167e12db3173717076c8723d
UssCringe <david.petur@gmail.com>
2022-12-01 12:59:29 +0100

    update

---
+++ components/NoResults.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface IProps {
+    text: string
+}
+
+const NoResults = ({ text }: IProps) => {
+  return (
+    <div>
+      noresults
+    </div>
+  )
+}
+
+export default NoResults
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,36 @@
-import type { NextPage } from "next"
+
 import axios from "axios"
+import NoResults from "../components/NoResults"
+import VideoCard from "../components/VideoCard"
+import { Video } from "../types"
 
+interface IProps {
+   videos: Video[]
+}
 
-const Home: NextPage = () => {
+const Home = ({ videos }: IProps) => {
+  console.log(videos)
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="flex flex-col gap-10 videos h-full">
+      {videos.length ? (
+        videos.map((video: Video) => (
+          <VideoCard post={video} key={video._id}/>
+        ))
+      ) : (
+        <NoResults text={"No videos"} />
+      )}
+    </div>
   )
 }
 
 export const getServerSideProps = async () => {
-  const response = await axios.get(`http://localhost:3000/api/post`)
-  console.log(response.data.name)
+  const { data } = await axios.get(`http://localhost:3000/api/post`)
+  
 
   return {
-    props: {}
+    props: {
+      videos: data
+    }
   }
 }
 
