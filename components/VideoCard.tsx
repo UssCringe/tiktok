4468098f595ca083f215f35c488c99b0ed8011d6
UssCringe <david.petur@gmail.com>
2022-12-04 20:55:51 +0100

    Update VideoCard.tsx

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 import Link from "next/link"
 
 import { HiVolumeUp, HiVolumeOff } from "react-icons/hi"
-import { BsPlay, BsFillPlayFill, BsFillPauseFill } from "react-icons/bs"
+import { BsFillPlayFill, BsFillPauseFill } from "react-icons/bs"
 import { GoVerified } from "react-icons/go"
 
 
@@ -121,7 +121,6 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
                 }
             </div>
           )}
-
 
         </div>
       </div>
