94d39abf348ad8fc61b1903331eee00b0fca32e7
UssCringe <david.petur@gmail.com>
2022-12-05 13:27:13 +0100

    mostly finished details page

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -76,6 +76,7 @@ const Upload = () => {
       }
 
       await axios.post("api/post", document)
+
       router.push("/")
     }
   }
@@ -166,7 +167,7 @@ const Upload = () => {
           <label className='text-md font-medium '>Choose a topic</label>
 
           <select
-            onChange={(e)=>{setCategory(e.target.value)}}
+            onChange={(e: any)=>{setCategory(e.target.value)}}
             className='outline-none lg:w-650 border-2 border-gray-200 text-md capitalize lg:p-4 p-2 rounded cursor-pointer'
           >
             {topics.map((item) => (
